d8ba9c043410178742fb94ee8d305331bc8708ed
Caylie Dawson <cnd@email.arizona.edu>
2019-02-22 14:46:04 -0800

    just updated it a little to make it look nicer, still working on understanding react so I'll keep working on tutorials as well

--- src/components/day2/day2.js
+++ src/components/day2/day2.js
@@ -10,63 +10,110 @@ const Day2 = props => (
     <h1 className="day2__artistName">Tame Impala</h1>
 
     <h3 className="day2__artistName__firstRow">Solange</h3>
+    <h3 className="day2__interpunct__firstRow">·</h3>
     <h3 className="day2__artistName__firstRow">Kid Cudi</h3>
+    <h3 className="day2__interpunct__firstRow">·</h3>
     <h3 className="day2__artistName__firstRow">Weezer</h3>
+    <h3 className="day2__interpunct__firstRow">·</h3>
     <h3 className="day2__artistName__firstRow">Aphex Twin</h3>
+    <h3 className="day2__interpunct__firstRow">·</h3>
     <h3 className="day2__artistName__firstRow">J Balvin</h3>
+    <h3 className="day2__interpunct__firstRow">·</h3>
     <h3 className="day2__artistName__firstRow">Billie Eilish</h3>
+    <h3 className="day2__interpunct__firstRow">·</h3>
     <h3 className="day2__artistName__firstRow">Bassnectar<br></br></h3>
 
+
     <h3 className="day2__artistName__secondRow">Four Tet</h3>
+    <h3 className="day2__interpunct__secondRow">·</h3>
     <h3 className="day2__artistName__secondRow">Christine and the Queens</h3>
+    <h3 className="day2__interpunct__secondRow">·</h3>
     <h3 className="day2__artistName__secondRow">Wiz Khalifa</h3>
+    <h3 className="day2__interpunct__secondRow">·</h3>
     <h3 className="day2__artistName__secondRow">Mac DeMarco</h3>
+    <h3 className="day2__interpunct__secondRow">·</h3>
     <h3 className="day2__artistName__secondRow">Bazzi</h3>
+    <h3 className="day2__interpunct__secondRow">·</h3>
     <h3 className="day2__artistName__secondRow">Maggie Rogers</h3>
+    <h3 className="day2__interpunct__secondRow">·</h3>
     <h3 className="day2__artistName__secondRow">Sheck Wes<br></br></h3>
 
     <h3 className="day2__artistName__thirdRow">Gryffin</h3>
+    <h3 className="day2__interpunct__thirdRow">·</h3>
     <h3 className="day2__artistName__thirdRow">Bob Moses</h3>
+    <h3 className="day2__interpunct__thirdRow">·</h3>
     <h3 className="day2__artistName__thirdRow">Virgil Abloh</h3>
+    <h3 className="day2__interpunct__thirdRow">·</h3>
     <h3 className="day2__artistName__thirdRow">Tale Of Us</h3>
+    <h3 className="day2__interpunct__thirdRow">·</h3>
     <h3 className="day2__artistName__thirdRow">Mr Eazi</h3>
+    <h3 className="day2__interpunct__thirdRow">·</h3>
     <h3 className="day2__artistName__thirdRow">Sabrina Claudio</h3>
+    <h3 className="day2__interpunct__thirdRow">·</h3>
     <h3 className="day2__artistName__thirdRow">Ty Segall & White Fence</h3>
+    <h3 className="day2__interpunct__thirdRow">·</h3>
     <h3 className="day2__artistName__thirdRow">Deep Dish<br></br></h3>
 
     <h3 className="day2__artistName__fourthRow">Smino</h3>
+    <h3 className="day2__interpunct__fourthThroughSeventhRow">·</h3>
     <h3 className="day2__artistName__fourthRow">FKJ</h3>
+    <h3 className="day2__interpunct__fourthThroughSeventhRow">·</h3>
     <h3 className="day2__artistName__fourthRow">SiR</h3>
+    <h3 className="day2__interpunct__fourthThroughSeventhRow">·</h3>
     <h3 className="day2__artistName__fourthRow">Idris Elba</h3>
+    <h3 className="day2__interpunct__fourthThroughSeventhRow">·</h3>
     <h3 className="day2__artistName__fourthRow">Parcels</h3>
+    <h3 className="day2__interpunct__fourthThroughSeventhRow">·</h3>
     <h3 className="day2__artistName__fourthRow">JAIN</h3>
+    <h3 className="day2__interpunct__fourthThroughSeventhRow">·</h3>
     <h3 className="day2__artistName__fourthRow">Soulection</h3>
+    <h3 className="day2__interpunct__fourthThroughSeventhRow">·</h3>
     <h3 className="day2__artistName__fourthRow">Turnover</h3>
+    <h3 className="day2__interpunct__fourthThroughSeventhRow">·</h3>
     <h3 className="day2__artistName__fourthRow">SALES</h3>
+    <h3 className="day2__interpunct__fourthThroughSeventhRow">·</h3>
     <h3 className="day2__artistName__fourthRow">Stephan Bodzin</h3>
+    <h3 className="day2__interpunct__fourthThroughSeventhRow">·</h3>
     <h3 className="day2__artistName__fourthRow">CloZee</h3>
+    <h3 className="day2__interpunct__fourthThroughSeventhRow">·</h3>
     <h3 className="day2__artistName__fourthRow">ARIZONA<br></br></h3>
 
     <h3 className="day2__artistName__fifthRow">Murda Beatz</h3>
+    <h3 className="day2__interpunct__fourthThroughSeventhRow">·</h3>
     <h3 className="day2__artistName__fifthRow">Jambinai</h3>
+    <h3 className="day2__interpunct__fourthThroughSeventhRow">·</h3>
     <h3 className="day2__artistName__fifthRow">Âme</h3>
+    <h3 className="day2__interpunct__fourthThroughSeventhRow">·</h3>
     <h3 className="day2__artistName__fifthRow">CHON</h3>
+    <h3 className="day2__interpunct__fourthThroughSeventhRow">·</h3>
     <h3 className="day2__artistName__fifthRow">Little Simz</h3>
+    <h3 className="day2__interpunct__fourthThroughSeventhRow">·</h3>
     <h3 className="day2__artistName__fifthRow">Adriatique</h3>
+    <h3 className="day2__interpunct__fourthThroughSeventhRow">·</h3>
     <h3 className="day2__artistName__fifthRow">Lee Burridge</h3>
+    <h3 className="day2__interpunct__fourthThroughSeventhRow">·</h3>
     <h3 className="day2__artistName__fifthRow">the Garden</h3>
+    <h3 className="day2__interpunct__fourthThroughSeventhRow">·</h3>
     <h3 className="day2__artistName__fifthRow">Agoria<br></br></h3>
 
     <h3 className="day2__artistName__sixthRow">Hop Along</h3>
+    <h3 className="day2__interpunct__fourthThroughSeventhRow">·</h3>
     <h3 className="day2__artistName__sixthRow">shame</h3>
+    <h3 className="day2__interpunct__fourthThroughSeventhRow">·</h3>
     <h3 className="day2__artistName__sixthRow">Superorganism</h3>
+    <h3 className="day2__interpunct__fourthThroughSeventhRow">·</h3>
     <h3 className="day2__artistName__sixthRow">serpentwithfeet</h3>
+    <h3 className="day2__interpunct__fourthThroughSeventhRow">·</h3>
     <h3 className="day2__artistName__sixthRow">Ookay</h3>
+    <h3 className="day2__interpunct__fourthThroughSeventhRow">·</h3>
     <h3 className="day2__artistName__sixthRow">Steady Holiday<br></br></h3>
 
     <h3 className="day2__artistName__seventhRow">Javiera Mena</h3>
+    <h3 className="day2__interpunct__fourthThroughSeventhRow">·</h3>
     <h3 className="day2__artistName__seventhRow">the Messthetics</h3>
+    <h3 className="day2__interpunct__fourthThroughSeventhRow">·</h3>
     <h3 className="day2__artistName__seventhRow">the Red Pears</h3>
+    <h3 className="day2__interpunct__fourthThroughSeventhRow">·</h3>
     <h3 className="day2__artistName__seventhRow">Heidi Lawden<br></br></h3>
   </section>
 )
